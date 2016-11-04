1aecc4e60637bf21e5402f3047d553a576959173
gyeshinwon <bluemirr5@gmail.com>
2016-11-04 14:32:08 +0900

    add slack post model

--- src/main/kotlin/kr.r2lab.myslackalarmbot/slack/SlackPost.kt
+++ src/main/kotlin/kr.r2lab.myslackalarmbot/slack/SlackPost.kt
@@ -6,11 +6,36 @@ import java.util.*
  * Created by gyeshinwon on 2016. 10. 29..
  *
  */
-data class SlackPost(
+data class NormalSlackPost(
     val username: String = "",
     val mrkdwn: Boolean = true,
     val channel: String = "#general",
     val text: String
+)
+
+
+data class AttachmentSlackPost(
+    val fallback: String,
+    val color: String,
+    val pretext: String,
+    val author_name: String,
+    val author_link: String,
+    val author_icon: String,
+    val title: String,
+    val title_link: String,
+    val text: String,
+    val image_url: String,
+    val thumb_url: String,
+    val footer: String,
+    val footer_icon: String,
+    val ts: Long,
+    val fields: List<AttachmentSlackPostField>
+)
+
+data class AttachmentSlackPostField(
+    val title: String,
+    val value: String,
+    val short: Boolean
 )
 
 class SlackPostDTO (
@@ -21,5 +46,5 @@ class SlackPostDTO (
     var text: String = "",
     var sendDate: Date? = null
 ){
-    fun toSlackPost() = SlackPost(username, mrkdwn, channel, text)
+    fun toSlackPost() = NormalSlackPost(username, mrkdwn, channel, text)
 }
